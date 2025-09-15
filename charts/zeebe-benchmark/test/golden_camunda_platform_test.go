1b8246af8a8de749aa5426b210987d5dde9bfb51
Christopher Kujawa <zelldon91@googlemail.com>
2025-09-15 12:36:22 +0200

    build: gofmt

--- charts/zeebe-benchmark/test/golden_camunda_platform_test.go
+++ charts/zeebe-benchmark/test/golden_camunda_platform_test.go
@@ -1,52 +1,52 @@
 package test
 
 import (
-    "benchmark-helm/charts/zeebe-benchmark/test/golden"
-    "path/filepath"
-    "strings"
-    "testing"
+	"benchmark-helm/charts/zeebe-benchmark/test/golden"
+	"path/filepath"
+	"strings"
+	"testing"
 
-    "github.com/gruntwork-io/terratest/modules/random"
-    "github.com/stretchr/testify/require"
-    "github.com/stretchr/testify/suite"
+	"github.com/gruntwork-io/terratest/modules/random"
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
 )
 
 func TestGoldenCamundaPlatformServiceMonitorDefaults(t *testing.T) {
-    // Test which allows to verify also parent chart templates
-    // This makes sure that properties are correctly set
-    // OR configurations have been changed
+	// Test which allows to verify also parent chart templates
+	// This makes sure that properties are correctly set
+	// OR configurations have been changed
 
-    chartPath, err := filepath.Abs("../")
-    require.NoError(t, err)
-    templateNames := []string{"orchestration-service-monitor"}
+	chartPath, err := filepath.Abs("../")
+	require.NoError(t, err)
+	templateNames := []string{"orchestration-service-monitor"}
 
-    for _, name := range templateNames {
-        suite.Run(t, &golden.TemplateGoldenTest{
-            ChartPath:      chartPath,
-            Release:        "benchmark-test",
-            Namespace:      "benchmark-" + strings.ToLower(random.UniqueId()),
-            GoldenFileName: "c8-" + name,
-            Templates:      []string{"charts/camunda-platform/templates/service-monitor/" + name + ".yaml"},
-        })
-    }
+	for _, name := range templateNames {
+		suite.Run(t, &golden.TemplateGoldenTest{
+			ChartPath:      chartPath,
+			Release:        "benchmark-test",
+			Namespace:      "benchmark-" + strings.ToLower(random.UniqueId()),
+			GoldenFileName: "c8-" + name,
+			Templates:      []string{"charts/camunda-platform/templates/service-monitor/" + name + ".yaml"},
+		})
+	}
 }
 
 func TestGoldenCamundaPlatformCoreDefaults(t *testing.T) {
-    // Test which allows to verify also parent chart templates
-    // This makes sure that properties are correctly set
-    // OR configurations have been changed
+	// Test which allows to verify also parent chart templates
+	// This makes sure that properties are correctly set
+	// OR configurations have been changed
 
-    chartPath, err := filepath.Abs("../")
-    require.NoError(t, err)
-    templateNames := []string{"service", "statefulset", "configmap"}
+	chartPath, err := filepath.Abs("../")
+	require.NoError(t, err)
+	templateNames := []string{"service", "statefulset", "configmap"}
 
-    for _, name := range templateNames {
-        suite.Run(t, &golden.TemplateGoldenTest{
-            ChartPath:      chartPath,
-            Release:        "benchmark-test",
-            Namespace:      "benchmark-" + strings.ToLower(random.UniqueId()),
-            GoldenFileName: "c8-core-" + name,
-            Templates:      []string{"charts/camunda-platform/templates/orchestration/" + name + ".yaml"},
-        })
-    }
+	for _, name := range templateNames {
+		suite.Run(t, &golden.TemplateGoldenTest{
+			ChartPath:      chartPath,
+			Release:        "benchmark-test",
+			Namespace:      "benchmark-" + strings.ToLower(random.UniqueId()),
+			GoldenFileName: "c8-core-" + name,
+			Templates:      []string{"charts/camunda-platform/templates/orchestration/" + name + ".yaml"},
+		})
+	}
 }
